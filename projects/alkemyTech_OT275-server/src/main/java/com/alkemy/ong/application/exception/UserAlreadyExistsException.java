48aee552dd0f3239fff280537d884617cd08acfb
SuelenFelix <suelenfelix@pop-os.localdomain>
2026-07-04 20:59:52 -0300

    corrige pom

--- projects/alkemyTech_OT275-server/src/main/java/com/alkemy/ong/application/exception/UserAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.alkemy.ong.application.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-
-  public UserAlreadyExistsException(String message) {
-    super(message);
-  }
-
-}
--- projects/alkemyTech_OT275-server/src/main/java/com/alkemy/ong/application/repository/IRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alkemy.ong.application.repository;
-
-import com.alkemy.ong.domain.Role;
-
-public interface IRoleRepository {
-
-  Role findRoleUser();
-
-}
